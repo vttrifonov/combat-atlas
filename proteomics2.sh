618e75526e7ddd4603f4fb4c8e50ea3cec743e8b
Trifonov, Vladimir [JANUS] <VTrifono@its.jnj.com>
2022-09-27 14:46:08 -0700

    proteomics

--- proteomics2.sh
+++ proteomics2.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
-msconvert=/tpp/root/bin/msconvert
+msconvert="docker run -it -v /tmp:/data --rm proteowizard/pwiz-skyline-i-agree-to-the-vendor-licenses wine msconvert"
 cache=$(pwd)/.cache/download/CBD-KEY-PROTEOMICS
 
 mkdir -p .cache/proteomics2
 cd .cache/proteomics2
 
 input=xxx.d.zip.raw
+
+unzip $input
+
+eval $msconvert --help
