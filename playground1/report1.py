f072a6567e2847ea6e984030c459066ee5616144
Vladimir Trifonov <vtrifono@its.jnj.com>
2022-10-07 18:07:54 +0000

    playground1

--- playground1/report1.py
+++ playground1/report1.py
@@ -32,104 +32,111 @@ print(
         geom_vline(xintercept=x2.bulk_mat.mean(), linetype='--')+
         labs(
             x='bulk (RPM)', y='pseudbulkd (RPM)', 
-            title=f'{x2.feature_id.iloc[0]} (R={x2[["bulk_mat", "pseudo_mat"]].corr().to_numpy()[0,1]:.2f})'
+            title=f'{x2.feature_id.iloc[0]} (R={x2[["bulk_mat", "pseudo_mat"]].corr().to_numpy()[0,1]:.2f})',
+            color='Source'
         )
 )
 
 
 # %%
 x2 = x3.isel(sample_id=1).to_dataframe().reset_index()
-x2['f'] = x2.feature_id.str.contains("^RPL|^RPS|^MT-", regex=True)
-x2 = x2.sort_values('f')
+x2['f'] = np.where(
+    x2.feature_id.str.contains("^RPL|^RPS", regex=True), 'RP',
+    np.where(
+        x2.feature_id.str.contains("^MT-", regex=True), 'MT',
+        np.where(
+            x2.feature_id.isin(['ACTB', 'B2M']), x2.feature_id,
+            np.nan    
+        )
+    )
+)
+
+x2 = x2.sort_values('f', na_position='first')
 
 #x2.query('f==False')[['feature_id', 'bulk_mat', 'pseudo_mat']].sort_values('pseudo_mat').tail(11).feature_id.to_list()
 
 print(
-    ggplot(x2)+aes('np.log10(bulk_mat)', 'np.log10(pseudo_mat)')+
-        geom_point(aes(color='f'), alpha=0.2)+
+    ggplot(x2)+aes('bulk_mat', 'pseudo_mat')+
+        geom_point(aes(color='f'), alpha=0.8)+
         geom_smooth(method='lm')+
         geom_abline(slope=1, intercept=0)+
         labs(
-            x='bulk (log10RPM)', y='pseudbulkd (log10RPM)',
-            color='RP|MT'
-        )
+            x='bulk (RPM)', y='pseudbulkd (RPM)',
+            color='mRNA'
+        )+
+        scale_x_log10()+scale_y_log10()
 )
 
 # %%
-from combat_atlas.sigs import sigs
-x5 = self.data3
-x6 = x5[['ES', 'NES', 'padj']].to_dataframe().reset_index()
-x6 = x6.sort_values('padj')
-x6 = x6.query('padj<0.005').reset_index(drop=True)
+def _():
+    from combat_atlas.sigs import sigs
+    x5 = self.data3
+    x6 = x5[['ES', 'NES', 'padj']].to_dataframe().reset_index()
+    x6 = x6.sort_values('padj')
+    x6 = x6.query('padj<0.005').reset_index(drop=True)
 
-x6[x6.sig.str.contains('_T_CELL|NEUTRO')].sort_values('ES')
+    x6[x6.sig.str.contains('_T_CELL|NEUTRO')].sort_values('ES')
 
-x7 = self.data3.sel(sig='GOBP_REGULATION_OF_NEUTROPHIL_ACTIVATION')
-x7 = x7.sel(gene=x7.leadingEdge.todense()==1)
-#x7 = xa.merge([x7, sigs.all1.rename(sig='sig1')], join='inner')
-#x7 = x7.sel(sig1=x7.set.sum(dim='gene').todense()>0)
-#x8 = x7.sig1.to_series().reset_index(drop=True)
-x7[['symbol', 'R']].to_dataframe().sort_values('R')
-
-# %%
-x2 = x3.sel(gene_id=x3.feature_id=='C5AR1').to_dataframe().reset_index()
-print(
-    ggplot(x2)+aes('clin_Source', 'bulk_mat')+
-        geom_violin(aes(fill='clin_Source'))+
-        geom_jitter(width=0.2)+
-        coord_flip()+
-        labs(x=f'{x2.feature_id.iloc[0]} bulk (RPM)')+
-        theme(legend_position='none')
-)
-
-print(
-    ggplot(x2)+aes('clin_Source', 'pseudo_mat')+
-        geom_violin(aes(fill='clin_Source'))+
-        geom_jitter(width=0.2)+
-        coord_flip()+
-        labs(x=f'{x2.feature_id.iloc[0]} psuedobulk (RPM)')+
-        theme(legend_position='none')
-)
+    x7 = self.data3.sel(sig='GOBP_REGULATION_OF_NEUTROPHIL_ACTIVATION')
+    x7 = x7.sel(gene=x7.leadingEdge.todense()==1)
+    #x7 = xa.merge([x7, sigs.all1.rename(sig='sig1')], join='inner')
+    #x7 = x7.sel(sig1=x7.set.sum(dim='gene').todense()>0)
+    #x8 = x7.sig1.to_series().reset_index(drop=True)
+    x7[['symbol', 'R']].to_dataframe().sort_values('R')
 
 # %%
-x2 = x3.sel(gene_id=x3.feature_id=='CD3G').to_dataframe().reset_index()
-print(
-    ggplot(x2)+aes('clin_Source', 'bulk_mat')+
-        geom_violin(aes(fill='clin_Source'))+
-        geom_jitter(width=0.2)+
-        coord_flip()+
-        labs(x=f'{x2.feature_id.iloc[0]} bulk (RPM)')+
-        theme(legend_position='none')
-)
+x2 = x3.sel(gene_id=x3.feature_id=='C5AR1')
+x2 = x2.to_dataframe().reset_index()
+x2 = x2[['sample_id', 'feature_id', 'pseudo_mat', 'bulk_mat', 'clin_Source']]
+x2 = x2.melt(id_vars=['sample_id', 'feature_id', 'clin_Source'])
+x2['variable'] = np.where(x2.variable=='pseudo_mat', 'pseudobulk', 'bulk')
 
 print(
-    ggplot(x2)+aes('clin_Source', 'pseudo_mat')+
+    ggplot(x2)+aes('clin_Source', 'value')+
         geom_violin(aes(fill='clin_Source'))+
-        geom_jitter(width=0.2)+
+        geom_jitter(width=0.2)+        
+        facet_grid('~variable', scales='free_x')+
+        labs(y=f'{x2.feature_id.iloc[0]} (RPM)', x='Source')+
         coord_flip()+
-        labs(x=f'{x2.feature_id.iloc[0]} psuedobulk (RPM)')+
         theme(legend_position='none')
 )
 
 # %%
-x2 = x3.sel(gene_id=x3.feature_id=='CEACAM8').to_dataframe().reset_index()
-print(
-    ggplot(x2)+aes('clin_Source', 'np.log10(bulk_mat+1)')+
-        geom_violin(aes(fill='clin_Source'))+
-        geom_jitter(width=0.1)+
-        coord_flip()+
-        labs(x=f'{x2.feature_id.iloc[0]} bulk (RPM)')+
-        theme(legend_position='none')
-)
+x2 = x3.sel(gene_id=x3.feature_id=='CD3G')
+x2 = x2.to_dataframe().reset_index()
+x2 = x2[['sample_id', 'feature_id', 'pseudo_mat', 'bulk_mat', 'clin_Source']]
+x2 = x2.melt(id_vars=['sample_id', 'feature_id', 'clin_Source'])
+x2['variable'] = np.where(x2.variable=='pseudo_mat', 'pseudobulk', 'bulk')
 
 print(
-    ggplot(x2)+aes('clin_Source', 'np.log10(pseudo_mat+1)')+
+    ggplot(x2)+aes('clin_Source', 'value')+
         geom_violin(aes(fill='clin_Source'))+
-        geom_jitter(width=0.2)+
+        geom_jitter(width=0.2)+        
+        facet_grid('~variable', scales='free_x')+
+        labs(y=f'{x2.feature_id.iloc[0]} (RPM)', x='Source')+
         coord_flip()+
-        labs(x=f'{x2.feature_id.iloc[0]} psuedobulk (RPM)')+
-        theme(legend_position='none')
+        theme(legend_position='none')+
+        scale_y_log10()
 )
+
+# %%
+x2 = x3.sel(gene_id=x3.feature_id=='CEACAM8')
+x2 = x2.to_dataframe().reset_index()
+x2 = x2[['sample_id', 'feature_id', 'pseudo_mat', 'bulk_mat', 'clin_Source']]
+x2 = x2.melt(id_vars=['sample_id', 'feature_id', 'clin_Source'])
+x2['variable'] = np.where(x2.variable=='pseudo_mat', 'pseudobulk', 'bulk')
+
+print(
+    ggplot(x2)+aes('clin_Source', 'value')+
+        geom_violin(aes(fill='clin_Source'))+
+        geom_jitter(width=0.2)+        
+        facet_grid('~variable', scales='free_x')+
+        labs(y=f'{x2.feature_id.iloc[0]} (log10RPM)', x='Source')+
+        coord_flip()+
+        theme(legend_position='none')+
+        scale_y_log10()
+)
+
 
 # %%
 x3 = self.data1.copy()
@@ -138,6 +145,7 @@ x3 = x3.sel(sample_id=x3.clin_Source!='COVID_HCW_MILD')
 
 # %%
 x2 = x3.sel(gene_id=x3.feature_id=='C5AR1').to_dataframe().reset_index()
+x2['cell_type'] = np.where(x2.cell_type.str.contains('Mono'), x2.cell_type, 'Other')
 print(
     ggplot(x2)+aes('clin_Source', '(pseudo_mat)')+
         geom_violin(aes(fill='clin_Source'))+
@@ -145,9 +153,10 @@ print(
         facet_grid('cell_type~.', scales='free_y')+
         labs(y=f'{x2.feature_id.iloc[0]} psuedobulk (RPM)')+            
         theme(
-            legend_position='none', figure_size=(4, 18*2),
+            legend_position='none', figure_size=(10, 5),
             axis_text_x = element_text(angle=45, hjust=1)
-        )
+        )+
+        scale_y_log10()        
 )
 
 # %%
